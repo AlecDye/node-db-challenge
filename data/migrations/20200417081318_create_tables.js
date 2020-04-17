ec0d6e33a6ef5fc226e1bacbf7a0ef375b49ca32
Alec Dye <alecdye@gmail.com>
2020-04-17 08:21:45 -0700

    Running server without crashing

---
+++ data/migrations/20200417081318_create_tables.js
@@ -0,0 +1,60 @@
+exports.up = function (knex) {
+  return (
+    knex.schema
+      // Projects table
+      .createTable("projects", (tbl) => {
+        tbl.integer("id").primary();
+        tbl.string("title").notNullable().unique();
+        tbl.string("project_desc");
+        tbl.boolean("completed").defaultTo(false);
+      })
+      // Tasks table
+      .createTable("tasks", (tbl) => {
+        tbl.integer("id").primary();
+        tbl.string("desc").notNullable().unique();
+        tbl.string("notes");
+        tbl.boolean("completed").defaultTo(false);
+        // Foreign key (projects)
+        tbl
+          .integer("project_id")
+          .notNullable()
+          .references("id")
+          .inTable("projects")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+      })
+      // Resources table
+      .createTable("resources", (tbl) => {
+        tbl.integer("id").primary();
+        tbl.string("name").notNullable().unique();
+        tbl.string("desc");
+      })
+      // Project to Resource junction table
+      .createTable("project_resources", (tbl) => {
+        tbl.integer("id").primary();
+        // Foreign keys (project & resources)
+        tbl
+          .integer("project_id")
+          .notNullable()
+          .references("id")
+          .inTable("projects")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+        tbl
+          .integer("resource_id")
+          .notNullable()
+          .references("id")
+          .inTable("resources")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+      })
+  );
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("project_resources")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("projects");
+};
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 
-const ProjectsRouter = require("./projects/projects-router.js");
+const ProjectsRouter = require("./api/projects-router.js");
 
 const server = express();
 
